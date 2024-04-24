78453da67fd0e2a02344cdff419ade8ad012b768
Jason Wilson <info@jasonleewilson.com>
2024-04-23 21:29:30 -0500

    update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className='App'>
       <main className='p-8'>
         <form name='contact' method='POST' data-netlify='true'>
-          <input type='hidden' name='contact' value='contact' />
+          <input type='hidden' name='form-name' value='contact' />
           <p>
             <label>
               Your Name: <input type='text' name='name' />
